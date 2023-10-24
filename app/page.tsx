884d39d8fe13671ccbe2a3ba06acd3262e4cde37
Nahtanoel <nath.guiquerro@gmail.com>
2023-10-24 14:20:13 +0200

    fix test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <>
-      <p>Nathaniel C est mon appli test nextjs</p>
+      <p>Nathaniel C mon appli test nextjs</p>
       </>
     </main>
   )
