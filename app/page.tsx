8fa646779820906de675aced034d5ad0e88e0d44
Nahtanoel <nath.guiquerro@gmail.com>
2023-10-24 15:16:06 +0200

    fix text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <>
-      <p>Nathaniel C mon appli test nextjs</p>
+      <p>Nathaniel ceci est mon appli test nextjs</p>
       </>
     </main>
   )
